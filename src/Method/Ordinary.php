64366ccd49e976f1894fe349de7834fa4bbc6523
Tychyna Volodymyr <tychinaphp@i.ua>
2020-01-29 02:26:56 +0200

    scr/Platon.php - class was created to form a request for processing. src/Ordinary.php - class for the formation of a simple (form) request in processing.

---
+++ src/Method/Ordinary.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Platon\Method;
+
+use InvalidArgumentException;
+
+
+class Ordinary extends Method
+{
+
+    protected $data;
+
+    public function getInputParams()
+    {
+        $result = [];
+
+        $this->verify();
+        $this->setEncData();
+
+        $result['data'] = $this->data;
+
+        foreach ($this->params as $k => $v) {
+            if (in_array($k, ['amount', 'currency', 'description', 'recurring'])) {
+                continue;
+            }
+            $result[$k] = $v;
+        }
+
+        return $result;
+    }
+
+    protected function verify()
+    {
+        foreach (['payment', 'amount', 'currency', 'description', 'url'] as $k) {
+            if (!isset($this->params[$k]) || empty($this->params[$k])) {
+                throw new InvalidArgumentException("Empty parameter: $k");
+            }
+        }
+
+        if (!preg_match('/^\d{1,6}\.\d{2}$/', $this->params['amount'])) {
+            throw new InvalidArgumentException("Invalid amount");
+        }
+
+        if (!preg_match('/^[A-Z]{3}$/', $this->params['currency'])) {
+            throw new InvalidArgumentException("Invalid currency");
+        }
+
+        if (!preg_match('https?://.+', $this->params['url'])) {
+            throw new InvalidArgumentException("Invalid url");
+        }
+    }
+
+    protected function setEncData()
+    {
+        $arr = [
+            'amount' => $this->params['amount'],
+            'currency' => $this->params['currency'],
+            'description' => $this->params['description'],
+        ];
+
+        if (!empty($this->params['recurring'])) {
+            $arr['recurring'] = 'recurring';
+        }
+
+        $this->data = base64_encode(json_encode($arr));
+    }
+
+    public function getSign($key, $password)
+    {
+        $sign = md5(strtoupper(
+            strrev($key)
+            . strrev($this->params['payment'])
+            . strrev($this->data)
+            . strrev($this->params['url'])
+            . strrev($password)
+        ));
+        return $sign;
+
+    }
+}
